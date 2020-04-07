ca5d33dceb189ae909cd117c6651ac2711f0a7df
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-04-07 22:17:57 +0900

    Update if-else

--- score.py
+++ score.py
@@ -1,3 +1,13 @@
 score = 80
-if score > 79:
-    print("合格です")
+if score > 78:
+    print("合格です。おめでとう！")
+else:
+    print("不合格。次回頑張りましょう。")
+
+score = 100
+if score == 100:
+    print("満点")
+elif score > 85:
+    print("合格！")
+else:
+    print("不合格。次回頑張りましょう。")
